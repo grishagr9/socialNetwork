a3732a4164d0afa337f6574d18208a69da16e786
grishagr9 <grisha-rnd10@yandex.ru>
2024-01-17 23:50:55 +0300

    tests for messager is done

--- Messager/src/main/java/org/example/Message.java
+++ Messager/src/main/java/org/example/Message.java
@@ -1,10 +1,12 @@
 package org.example;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
 public class Message {
     private String text;
     private Person from;
--- Messager/src/test/java/org/example/SendibleTest.java
+++ Messager/src/test/java/org/example/SendibleTest.java
@@ -1,16 +1,53 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class SendibleTest {
 
-    @Test
-    void createMessage() {
+    @Mock
+    Sendible sendible;
+
+    @BeforeEach
+    void before(){
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    void sendMessage() {
+    void createMessage() {
+        //when
+        Person person1 = new Person();
+        Person person2 = new Person();
+        when(sendible.createMessage(person1, person2, "Hello"))
+                .thenReturn(new Message("Hello", person1, person2));
+
+        Message message = sendible.createMessage(person1, person2, "Hello");
+
+        Assertions.assertEquals(message.getText(), "Hello");
+    }
+
+    @Test
+    void sendMessage_SamePersonSend_Fail() {
+        Person person = new Person();
+        Message message = new Message("Hello", person, person);
+        when(sendible.sendMessage(message)).thenReturn(false);
+
+        Assertions.assertFalse(sendible.sendMessage(message));
+    }
+
+    @Test
+    void sendMessage_TwoPersonSend_Success() {
+        Person person1 = new Person();
+        Person person2 = new Person();
+        Message message = new Message("Hello", person1, person2);
+        when(sendible.sendMessage(message)).thenReturn(true);
+
+        Assertions.assertTrue(sendible.sendMessage(message));
     }
 }
